b7875027760caa3b9c4f037a86c04ad185c0a033
ivandgc <ivandgc@hotmail.com>
2017-07-24 13:56:25 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,22 @@
+require 'byebug'
+
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    set_student
+    @status =  (@student.active ? "active" : "inactive")
+  end
+
+  def activate
+    set_student
+    @flip = (@student.active ? false : true)
+    @student.update(active: @flip )
+    redirect_to student_path(@student)
   end
 
   private
